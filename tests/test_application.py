b7ef0f7e3c92c051eaec420e887a398c18ea416f
Alex Mathew <alexmathew003@gmail.com>
2018-12-13 17:41:25 +0530

    Move tests to new vial project

---
+++ tests/test_application.py
@@ -0,0 +1,22 @@
+from nose import with_setup
+from nose.tools import (assert_equal, assert_in, assert_is_none,
+                        assert_is_not_none)
+
+from vial.server.router import Application
+
+app = Application('test_runner')
+
+
+def setup():
+    @app.route(methods=['GET'], path='/$')
+    def route1():
+        return '1'
+
+
+@with_setup(setup)
+def test_application_routes():
+    assert_in('get', app._routes)
+    assert_is_none(app.validate_route('get', '/'))
+    assert_is_not_none(app.validate_route('post', '/'))
+    assert_is_not_none(app.validate_route('get', '/1'))
+    assert_equal('1', app.get_controller('get', '/')[0]())
---
+++ tests/test_postgres_engine.py
@@ -0,0 +1,37 @@
+from nose.tools import assert_equal, assert_in
+
+from vial.db.postgresql import Postgresql
+
+pg = Postgresql(dbname='', user='', password='', host='', port='')
+
+
+def test_create():
+    assert_in('id SERIAL PRIMARY KEY', pg._create_query('test', serial=True))
+    assert_in(
+        'name varchar PRIMARY KEY NOT NULL',
+        pg._create_query('test', fields={
+            'name': {'type': 'str', 'primary': True, 'not_null': True}
+        })
+    )
+
+
+def test_insert():
+    assert_in('(name, number)', pg._insert_query('test', fields=['name', 'number']))
+    assert_in('(%s, %s)', pg._insert_query('test', fields=['name', 'number']))
+
+
+def test_select():
+    assert_in('SELECT * FROM test', pg._select_query('test'))
+    assert_in('name, number', pg._select_query('test', fields=['name', 'number']))
+    assert_in("WHERE id=1 AND name='abc'", pg._select_query('test', where={'id': 1, 'name': 'abc'}))
+    assert_in('OFFSET 10', pg._select_query('test', offset=10))
+    assert_in('LIMIT 10', pg._select_query('test', limit=10))
+
+
+def test_update():
+    assert_in('WHERE id=(%s) AND name=(%s)', pg._update_query('test', where={'id': 1, 'name': 'abc'}))
+    assert_in('SET name=(%s), number=(%s)', pg._update_query('test', updation={'name': 'abc', 'number': 123}))
+
+
+def test_delete():
+    assert_in('WHERE id=(%s) AND name=(%s)', pg._delete_query('test', where={'id': 1, 'name': 'abc'}))
